fb1eb7c54aacafc024f5838e3cf496c3551bdac4
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2023-08-15 19:02:41 -0500

    Variables y metodos Backend Php

---
+++ 03_metodo_get.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Digite información: </h1>
+    <form action="03_metodo_get.php" method="get">
+        Nombre:
+        <input type="text" name="nombre" id="01">
+        <br><br>
+        Fecha:
+        <input type="date" name="fecha" id="02">
+        <br><br>
+        Observación:
+        <input type="text" name="obs" id="">
+        <br><br>
+        <input type="submit" value="Enviar">
+    </form>
+    <h2>
+        información enviada es:
+        <?php
+
+        print_r( $_GET);
+
+        ?>
+    </h2>
+</body>
+
+</html>
---
+++ variable.php
@@ -0,0 +1,18 @@
+<?php
+
+//Tipos de Variables
+
+$nombre= "Pedro";
+$edad= 24;
+$double= 25.3;
+$boolean= false;
+$nombreCurso="Backend";
+
+//Concatenar .
+$concatenar= $nombre. '<br>'. $edad .'<br>'. $double; 
+echo $concatenar;
+
+echo '<br>' . $nombre.  ", Hola mundo.";
+
+
+?>
